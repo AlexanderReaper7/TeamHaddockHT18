c390a3ce13bd53b12c32a26e266d3ab0967cd7bc
Alexander Öberg <stativetroller7@gmail.com>
2018-12-11 06:09:55 +0100

    asd

--- TeamHaddock/TeamHaddock/TeamHaddock/UserInterface.cs
+++ TeamHaddock/TeamHaddock/TeamHaddock/UserInterface.cs
@@ -16,6 +16,7 @@ namespace TeamHaddock
         // Edited by Noble 12-10 
         private static Rectangle heathBarBackgroundSource = new Rectangle(0, 0, 364, 100);
         private static Rectangle healthBarFillerSource = new Rectangle(57, 100, 239, 23);
+
         public static void LoadContent(ContentManager content)
         {
             healthBar = content.Load<Texture2D>(@"Textures/ActiveObjects/HealthBar");
@@ -24,10 +25,10 @@ namespace TeamHaddock
         public static void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
+            // Draw Healthbar filler
+            spriteBatch.Draw(healthBar, new Vector2(healthBarPosition.X + healthBarFillerSource.X, healthBarPosition.Y + 39), new Rectangle(healthBarFillerSource.X, healthBarFillerSource.Y, (int)(healthBarFillerSource.Width * ((float)InGame.player.Health / (float)Player.maxHealth)), healthBarFillerSource.Height), Color.White);
             // Draw health bar background
             spriteBatch.Draw(healthBar, healthBarPosition, heathBarBackgroundSource, Color.White);
-            // Draw Healthbar filler
-            spriteBatch.Draw(healthBar, new Vector2(healthBarPosition.X + healthBarFillerSource.X, healthBarPosition.Y + healthBarFillerSource.Y), new Rectangle(healthBarFillerSource.X, healthBarFillerSource.Y, healthBarFillerSource.Width * (InGame.player.Health / Player.maxHealth), healthBarFillerSource.Height) , Color.White);
             // Draw wave number
             spriteBatch.End();
         }
